174609b5f69c7386aac71f38c91c6daeef644de7
Ekzarcio <evg1093@gmail.com>
2020-10-17 21:11:10 +0300

    Add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  root to: "home#index"
+
   get 'home/index'
 
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
 
-  resources :articles
+  resources :articles do
+    resources :comments, only: :create
+  end
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
